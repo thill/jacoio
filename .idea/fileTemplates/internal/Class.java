15401e0242ca5c35f302558548162c51a5e53e71
Eric Thill <code@thill.io>
2019-06-06 11:02:34 -0500

    .idea code style

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,14 @@
+/*
+ * Licensed under the Apache License, Version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * ${NAME}
+ *
+ * @author ${USER}
+ */
+public class ${NAME} {
+}
